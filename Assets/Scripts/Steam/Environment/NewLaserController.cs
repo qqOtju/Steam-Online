8b51041ee38c44ac6e1d03d49de49d17767dd6c7
Otju <oleg.sid.14@gmail.com>
2023-07-21 13:49:37 +0300

    Steam | Code refactor

---
+++ Assets/Scripts/Steam/Environment/NewLaserController.cs
@@ -0,0 +1,53 @@
+﻿using Mirror;
+using Steam.Interface;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Steam.Environment
+{
+    public class NewLaserController : NetworkBehaviour
+    {
+        [SerializeField] private FloatConstant _laserDamage;
+        [SerializeField] private Transform _startTransform;
+        [SerializeField] private GameObject _laserObj;
+        [SerializeField] private float _timeBeforeDamage = 1f;
+
+        private Transform _laserTransform;
+        private Vector3 _startPos;
+        private bool _damaging;
+        private float _time;
+
+        [ServerCallback]
+        private void Awake()
+        {
+            _laserTransform = _laserObj.transform;
+            _startPos = _startTransform.position;
+        }
+
+        [ClientRpc]
+        private void FixedUpdate()
+        {
+            if(_damaging)
+                _time += Time.deltaTime;
+            if (Physics.Raycast(_startTransform.position, _startTransform.forward, out var hit))
+            {
+                var distance = Vector3.Distance(_startPos, hit.point) / 2;
+                _laserTransform.position = Vector3.Lerp(_startPos, hit.point, 0.5f);
+                var localScale = _laserTransform.localScale;
+                localScale = new Vector3(localScale.x, distance, localScale.z);
+                _laserTransform.localScale = localScale;
+                if (!hit.collider.TryGetComponent<IDamageable>(out var damageable)) return;
+                _damaging = true;
+                if (!(_time >= _timeBeforeDamage)) return;
+                _time = 0f;
+                damageable.GetDamage(_laserDamage.Value);
+            }
+            else
+            {
+                _damaging = false;
+                _time = 0f;
+                _laserTransform.localScale = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Steam/UI/UIAnimationsController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Steam.UI
+{
+    public class UIAnimationsController : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _buttons;
+        [SerializeField] private float _animTime = 0.15f;
+        [SerializeField] private float _difference = 0.07f;
+
+        private readonly Dictionary<GameObject, ButtonAnim> _buttonStats = new();
+        private WaitForSeconds _wfs;
+
+        private void Start()
+        {
+            _wfs = new(_animTime * 2);
+            
+            foreach (var btn in _buttons)
+            {
+                var rectTransform = btn.transform.GetChild(1).GetComponent<RectTransform>();
+                _buttonStats.Add(btn, new ButtonAnim()
+                {
+                    rectTransform = rectTransform,
+                    baseAnchorMax = rectTransform.anchorMax,
+                    baseAnchorMin = rectTransform.anchorMin,
+                    clickedAnchorMax = rectTransform.anchorMax - new Vector2(0,_difference),
+                    clickedAnchorMin =  rectTransform.anchorMin - new Vector2(0,_difference),
+                    interactable = true
+                });
+            }
+        }
+
+        public void BtnPress(GameObject btn)
+        {
+            StartCoroutine(Animate(btn));
+        }
+        
+        private IEnumerator Animate(GameObject btn)
+        {
+            var info = _buttonStats[btn];
+            if(!info.interactable) yield break;
+            info.interactable = false;
+            LeanTween.value(0, 1, _animTime).setOnUpdate(value =>
+            {
+                info.rectTransform.anchorMax = new Vector2(info.rectTransform.anchorMax.x,
+                    Mathf.Lerp(info.baseAnchorMax.y,  info.clickedAnchorMax.y, value));
+                info.rectTransform.anchorMin = new Vector2(info.rectTransform.anchorMin.x,
+                    Mathf.Lerp(info.baseAnchorMin.y, info.clickedAnchorMin.y, value));
+            }).setOnComplete(_ =>
+            {
+                LeanTween.value(0, 1, _animTime).setOnUpdate(value =>
+                {
+                    info.rectTransform.anchorMax = new Vector2( info.rectTransform.anchorMax.x,
+                        Mathf.Lerp(info.clickedAnchorMax.y, info.baseAnchorMax.y, value));
+                    info.rectTransform.anchorMin = new Vector2( info.rectTransform.anchorMin.x,
+                        Mathf.Lerp(info.clickedAnchorMin.y, info.baseAnchorMin.y, value));
+                }).setOnComplete(_ =>
+                {
+                    info.interactable = true;
+                });
+            });
+            yield return _wfs;
+        }
+    }
+
+    public struct ButtonAnim
+    {
+        public RectTransform rectTransform;
+        public Vector2 baseAnchorMax;
+        public Vector2 baseAnchorMin;
+        public Vector2 clickedAnchorMax;
+        public Vector2 clickedAnchorMin;
+        public bool interactable;
+    }
+}
