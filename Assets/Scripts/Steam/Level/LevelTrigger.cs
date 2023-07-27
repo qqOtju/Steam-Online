c4353af566118ca2e8e9b5e3d70843b88ed5626d
Otju <oleg.sid.14@gmail.com>
2023-07-27 19:56:19 +0300

    Steam | Demo done

---
+++ Assets/Scripts/Steam/Level/LevelTrigger.cs
@@ -0,0 +1,36 @@
+﻿using Mirror;
+using MyMirror;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Steam.Level
+{
+    [SelectionBase]
+    [RequireComponent(typeof(Collider))]
+    public class LevelTrigger : MonoBehaviour
+    {
+        [SerializeField] [Scene] private string _nextScene;
+
+        private MyNetworkManager _networkManager;
+        private bool _sceneChange;
+
+        private MyNetworkManager Manager
+        {
+            get
+            {
+                if (_networkManager != null) return _networkManager;
+                return _networkManager = NetworkManager.singleton as MyNetworkManager;
+            }
+        }
+
+        [Server]
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_sceneChange && other.CompareTag("Player"))
+            {
+                _sceneChange = true;
+                Manager.ServerChangeScene(_nextScene.SceneName());    
+            }
+        }
+    }
+}
